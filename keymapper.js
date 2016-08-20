38f4849e1f0fe3e2fd09fdcffd61ece0ebf2169a
hackape <muse.vincent@gmail.com>
2016-08-20 21:31:57 +0800

    add keyCodeMap

--- keymapper.js
+++ keymapper.js
@@ -1,3 +1,5 @@
+import {keyCodeToKey, keyToKeyCode} from './keyCodeMap';
+
 // Polyfill from https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/CustomEvent
 (function () {
   if ( typeof window.CustomEvent === "function" ) return false;
@@ -7,28 +9,9 @@
     evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
     return evt;
    }
-
   CustomEvent.prototype = window.Event.prototype;
-
   window.CustomEvent = CustomEvent;
 })();
-
-const KEYCODE = {
-  backspace: 8, tab: 9, clear: 12,
-  enter: 13, 'return': 13,
-  esc: 27, escape: 27, space: 32,
-  left: 37, up: 38,
-  right: 39, down: 40,
-  del: 46, 'delete': 46,
-  home: 36, end: 35,
-  pageup: 33, pagedown: 34,
-  ',': 188, '.': 190, '/': 191,
-  '`': 192, '-': 189, '=': 187,
-  ';': 186, '\'': 222,
-  '[': 219, ']': 221, '\\': 220
-};
-for(let k=1;k<20;k++) {KEYCODE['f'+k] = 111+k;} // f1 - f20
-
 
 var _lastTimeoutId;
 var _keyBuffer = [];
