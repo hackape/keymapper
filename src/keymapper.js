46cebe42400cfe3d207cfa6f797d2b9a4454c30f
hackape <muse.vincent@gmail.com>
2016-08-22 15:07:21 +0800

    down compile to ES5

--- src/keymapper.js
+++ src/keymapper.js
@@ -1,6 +1,16 @@
-import {keyCodeToKey, keyToKeyCode} from './keycodes';
+'use strict';
 
-var _keymaps = {global:{}};
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _keycodes = require('./keycodes');
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var _keymaps = { global: {} };
 var _commandHandlers = {};
 var _lastTimeoutId;
 var _combinator = '+';
@@ -8,7 +18,7 @@ var _context = 'global';
 var _commandBuffer = {
   keys: [],
   command: null,
-  reset: function() {
+  reset: function () {
     this.keys.length = 0;
     this.command = null;
   },
@@ -16,22 +26,22 @@ var _commandBuffer = {
     this.command = command;
   }
 };
-const _modifiersList = ['cmd','ctrl','shift','alt'];
-const _modifiers = {
+var _modifiersList = ['cmd', 'ctrl', 'shift', 'alt'];
+var _modifiers = {
   cmd: false,
   ctrl: false,
   shift: false,
   alt: false,
-  reset: function() {
+  reset: function () {
     this.cmd = this.ctrl = this.shift = this.alt = false;
   },
   toString: modifiersToString
 };
 
 function modifiersToString(e) {
-  let _this = e || this;
-  let modifiers = _modifiersList.reduce(function(ret, modifier) {
-    var modKey = e ? modifier+'Key' : modifier;
+  var _this = e || this;
+  var modifiers = _modifiersList.reduce(function (ret, modifier) {
+    var modKey = e ? modifier + 'Key' : modifier;
     if (modKey === 'cmdKey') modKey = 'metaKey';
     if (_this[modKey]) ret.push(modifier);
     return ret;
@@ -39,14 +49,14 @@ function modifiersToString(e) {
   return modifiers.join(_combinator);
 }
 
-function keyEventToKeyCombination (e) {
-  let modString = modifiersToString(e);
+function keyEventToKeyCombination(e) {
+  var modString = modifiersToString(e);
   if (!modString) modString = _modifiers.toString();
   _modifiers.reset();
   if (modString) {
-    return [modString, keyCodeToKey[e.keyCode]].join(_combinator);
+    return [modString, _keycodes.keyCodeToKey[e.keyCode]].join(_combinator);
   } else {
-    return keyCodeToKey[e.keyCode];
+    return _keycodes.keyCodeToKey[e.keyCode];
   }
 }
 
@@ -54,7 +64,7 @@ function handleKeyEvent(e) {
   clearTimeout(_lastTimeoutId);
 
   switch (e.key) {
-    case 'Meta' || 'OS' :
+    case 'Meta' || 'OS':
       _modifiers.cmd = true;
       break;
     case 'Control':
@@ -74,8 +84,8 @@ function handleKeyEvent(e) {
       });
   }
 
-  _lastTimeoutId = setTimeout(()=>{
-    consumeCommandBuffer()
+  _lastTimeoutId = setTimeout(function () {
+    consumeCommandBuffer();
   }, 500);
 }
 
@@ -89,7 +99,9 @@ function consumeCommandBuffer() {
 }
 
 function dispatchCommand(command) {
-  var {keys, context} = command;
+  var keys = command.keys;
+  var context = command.context;
+
   if (!_keymaps[context]) context = 'global';
   if (!_keymaps[context][keys]) return;
   command.type = _keymaps[context][keys];
@@ -101,79 +113,94 @@ function dispatchCommand(command) {
 }
 
 function handleCommand(command) {
-  var handler = (command.type) ? _commandHandlers[command.type] : command.$$handler;
+  var handler = command.type ? _commandHandlers[command.type] : command.$$handler;
   if (!handler) return;
   handler(command);
 }
 
-function normalizeKeys (keys) {
+function normalizeKeys(keys) {
   keys = keys.toLowerCase().replace(/\s/g, '');
-  const keyCombos = keys.split(',');
-  return keyCombos.map( keyCombo => {
+  var keyCombos = keys.split(',');
+  return keyCombos.map(function (keyCombo) {
     var pseudoKeyEvent = {};
-    keyCombo.split(_combinator).forEach( key => {
+    keyCombo.split(_combinator).forEach(function (key) {
       if (_modifiersList.indexOf(key) > -1) {
         if (key == 'cmd') key = 'meta';
-        pseudoKeyEvent[`${key}Key`] = true;
+        pseudoKeyEvent[key + 'Key'] = true;
       } else {
-        pseudoKeyEvent.keyCode = keyToKeyCode[key];
+        pseudoKeyEvent.keyCode = _keycodes.keyToKeyCode[key];
       }
     });
-    if (typeof pseudoKeyEvent.keyCode != 'number')
-      throw Error(`Keymapper: Unrecognized key combination \`${keyCombo}\``);
+    if (typeof pseudoKeyEvent.keyCode != 'number') throw Error('Keymapper: Unrecognized key combination `' + keyCombo + '`');
     return keyEventToKeyCombination(pseudoKeyEvent);
   }).join(',');
 }
 
-export default class Keymapper {
-  static setContext(context) {
-    _context = context;
-  }
+var Keymapper = function () {
+  _createClass(Keymapper, null, [{
+    key: 'setContext',
+    value: function setContext(context) {
+      _context = context;
+    }
+  }]);
 
-  constructor(config={}) {
+  function Keymapper() {
+    var config = arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
+
+    _classCallCheck(this, Keymapper);
+
+    this.setContext = Keymapper.setContext;
+
     if (!Keymapper.$$singleton) {
-      const {combinator} = config;
+      var combinator = config.combinator;
+
       if (combinator && typeof combinator === 'string') {
-        if (combinator!=='+' && combinator!=='-'){ throw Error('Keymapper: '+
-          'Unrecognized combinator, only "+" or "-" is supported.')}
+        if (combinator !== '+' && combinator !== '-') {
+          throw Error('Keymapper: ' + 'Unrecognized combinator, only "+" or "-" is supported.');
+        }
         _combinator = combinator;
       }
 
       this.add = this.map;
 
       window.addEventListener('keydown', handleKeyEvent);
-      Object.defineProperty(Keymapper, '$$singleton', {value: this});
+      Object.defineProperty(Keymapper, '$$singleton', { value: this });
     } else {
       return Keymapper.$$singleton;
     }
   }
 
-  map(keys, descriptor) {
-    if (typeof descriptor === 'string') {
-      var commandType = descriptor;
-      descriptor = {command: commandType, context: 'global'};
-    } else if (typeof descriptor === 'function') {
-      var handler = descriptor;
-      descriptor = {command: handler, context: 'global'};
+  _createClass(Keymapper, [{
+    key: 'map',
+    value: function map(keys, descriptor) {
+      if (typeof descriptor === 'string') {
+        var commandType = descriptor;
+        descriptor = { command: commandType, context: 'global' };
+      } else if (typeof descriptor === 'function') {
+        var handler = descriptor;
+        descriptor = { command: handler, context: 'global' };
+      }
+      if (!descriptor.command || !descriptor.context) return;
+      keys = normalizeKeys(keys);
+      if (!_keymaps[descriptor.context]) _keymaps[descriptor.context] = {};
+      _keymaps[descriptor.context][keys] = descriptor.command;
     }
-    if (!descriptor.command || !descriptor.context) return;
-    keys = normalizeKeys(keys);
-    if (!_keymaps[descriptor.context]) _keymaps[descriptor.context] = {};
-    _keymaps[descriptor.context][keys] = descriptor.command;
-  }
+  }, {
+    key: 'loadKeymaps',
+    value: function loadKeymaps(keymaps) {}
+  }, {
+    key: 'addCommandHandler',
+    value: function addCommandHandler(commandType, commandHandler) {
+      _commandHandlers[commandType] = commandHandler;
+    }
+  }, {
+    key: 'loadCommandHandlers',
+    value: function loadCommandHandlers(commandHandlers, override) {
+      _commandHandlers = override ? commandHandlers : Object.assign(_commandHandlers, commandHandlers);
+    }
+  }]);
 
-  loadKeymaps(keymaps) {}
+  return Keymapper;
+}();
 
-  addCommandHandler(commandType, commandHandler) {
-    _commandHandlers[commandType] = commandHandler;
-  }
-
-  loadCommandHandlers(commandHandlers, override) {
-    _commandHandlers = override ? commandHandlers
-      : Object.assign(_commandHandlers, commandHandlers);
-  }
-
-  setContext = Keymapper.setContext
-
-}
-
+exports.default = Keymapper;
